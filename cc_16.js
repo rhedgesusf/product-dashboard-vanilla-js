4173913978bb287e175c6a4f8363f228bb74421b
Riley Hedges <rhedges1@usf.edu>
2025-03-27 18:46:47 -0400

    Task 2 - implement fetchProductsThen using .then() and .catch()

---
+++ cc_16.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////
+// Task 2: Fetch Products with .then() //
+/////////////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Fetch Products with .then()");
+
+function fetchProductsThen() {
+    fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(products => {
+        products.forEach(product => {
+            console.log(product.fields.name)
+        })
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    })
+}
+
+fetchProductsThen();
