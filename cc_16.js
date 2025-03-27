9d8f3a733ccfa12a8c5e6bb4215ff944a3ca7975
Riley Hedges <rhedges1@usf.edu>
2025-03-27 19:10:11 -0400

    Task 3 - add fetchProductsAsync using async.await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -2,25 +2,61 @@
 // Task 2: Fetch Products with .then() //
 /////////////////////////////////////////
 
-console.log("--------------------------------------");
-console.log("Task 2: Fetch Products with .then()");
-
 function fetchProductsThen() {
-    fetch('https://www.course-api.com/javascript-store-products')
-    .then(response => {
-        if(!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+  fetch("https://www.course-api.com/javascript-store-products")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-    .then(products => {
-        products.forEach(product => {
-            console.log(product.fields.name)
-        })
+    .then((products) => {
+      products.forEach((product) => {
+        console.log(product.fields.name);
+      });
     })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
 fetchProductsThen();
+
+/////////////////////////////////////////////
+// Task 3: Fetch products with async/await //
+/////////////////////////////////////////////
+
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch(
+      "https://www.course-api.com/javascript-store-products"
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+fetchProductsAsync();
+
+function displayProducts(products) {
+  const container = document.getElementById("product-container");
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    // set class and id
+    card.setAttribute("class", "product-card");
+    card.innerHTML = `<div><h3>${product.fields.name}</h3>
+      <p>$${product.fields.price}</p>
+      <img src='${product.fields.image[0].url}' height='150'></img>
+      </div>`;
+    container.appendChild(card);
+  });
+}
+
+function handleError(error) {
+  console.error("Error:", error.message);
+}
